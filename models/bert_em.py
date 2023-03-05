16ae4e098643d4a4fb57934afa2c513cb6f7fd1c
Aboubacar Tuo <aboubacar.tuo@cea.fr>
2023-03-05 22:07:38 +0100

    add trigger similarity

--- models/bert_em.py
+++ models/bert_em.py
@@ -100,6 +100,6 @@ class BERT_EM(BertPreTrainedModel):
         a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
         a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
         b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
-        sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1)).mean(dim=-1)
+        sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1)).mean().mean()
         print(sim_mt)
         return sim_mt
